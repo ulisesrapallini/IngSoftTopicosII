67400f4af37b0f673fa29f7df0d07c2b28bb9adb
ulisesrapallini <ulises.rapallini@gmail.com>
2021-02-19 19:07:46 -0300

    19-02-2021-19-07

--- Archive_2/event-app-backend/src/controllers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ping.controller';
-export * from './event.controller';
-export * from './item.controller';
--- Archive_2/event-app-backend/src/datasources/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './locadb.datasource';
-export * from './mongodb.datasource';
