1d44ca4b643a616159215c3f026ab2bd02ce3d49
ulisesrapallini <ulises.rapallini@gmail.com>
2021-02-20 00:14:10 -0300

    20-02-2021-00-14

--- prueba/prueba/src/app/app.module.ts
+++ prueba/prueba/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PruebacentralComponent } from './pruebacentral/pruebacentral.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PruebacentralComponent } from './pruebacentral/pruebacentral.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
